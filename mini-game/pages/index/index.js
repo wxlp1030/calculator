5b505a37735257b99197bad36a4343ef535c3d17
ysywy <1300343646@qq.com>
2022-09-29 20:36:06 +0800

    计算器0.0.1版本

--- mini-game/pages/index/index.js
+++ mini-game/pages/index/index.js
@@ -4,8 +4,12 @@ const app = getApp()
 
 Page({
   data: {
-    number : 0,
+    number: 0,
+    next: 0,
     hasChar: false,
+    prev: 0,
+    first: 0,
+    type:'',
     numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '=', '.'],
     chars: ['+', '-', '*', '/']
   },
@@ -13,15 +17,51 @@ Page({
   },
   click(e) {
     const value = e.currentTarget.dataset.value
-    console.log(value);
-    if (this.data.chars.indexOf(value) != -1) {
+    if (this.data.chars.indexOf(value) != -1 || value == '=') {
       this.setData({
-        hasChar: true
+        hasChar: true,
       })
-      
     }
-    this.setData({
-      number : this.data.number + value + ''
-    })
+    if (!this.data.hasChar) {
+      this.setData({
+        number: this.data.number + value + ''
+      })
+    } else {
+      let nu = this.data.number + value;
+      const second = parseFloat(nu.substr(0, nu.length - 1))
+      const first = parseFloat(nu.substr(0, nu.length - 1))
+      if (value == '=') {
+        this.setData({
+          prev : this.data.prev + nu
+        })
+        if (this.data.first) {
+          nu = this.cacule(this.data.first, second, this.data.type)
+        }
+        this.setData({
+          number: nu
+        })
+      } else {
+        this.setData({
+          next: this.data.next + value + '',
+          prev: nu,
+          hasChar: false,
+          first,
+          type: value,
+          number: ''
+        })
+      }
+    }
+  },
+  cacule(first, second, type) {
+    switch (type) {
+      case '+':
+        return first + second;
+      case '-':
+        return first - second;
+      case '*':
+        return first * second;
+      case '/':
+        return first / second;
+    }
   }
 })
